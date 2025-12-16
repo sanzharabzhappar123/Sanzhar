adc77ab8d3807b74a715e5013ad85bc1fe76cba9
Sanzhar <izikatka615@gmail.com>
2025-12-16 22:44:08 +0500

    Task 2

---
+++ Task2.py
@@ -0,0 +1,9 @@
+Salary1 = int(input("firstsalary"))
+Salary2 = int(input("secondsalary"))
+Salary3 = int(input("thirdsalary"))
+
+a = max(Salary1, Salary2, Salary3)
+b = min(Salary1, Salary2, Salary3)
+
+d = a - b
+print(d)
