9ac673433d8a362c320ba0ec0fc6dc50fc68e05d
Sanzhar <izikatka615@gmail.com>
2025-12-16 22:42:28 +0500

    Task 1

---
+++ Task1.py
@@ -0,0 +1,7 @@
+first_name = input("name")
+last_name = input("last name? ")
+age = input("How old are you? ")
+phone_number = input("Your phone number? ")
+print(f"Your first name, last name: {first_name} {last_name}")
+print(f"Your age: {age}")
+print(f"Your phone number: {phone_number}")
