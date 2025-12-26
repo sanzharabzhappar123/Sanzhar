957596e448d09914b401e9da4b10d5ae946e3075
Sanzhar <izikatka615@gmail.com>
2025-12-26 17:26:29 +0500

    Task4 Assik2

---
+++ Task4_Assik2.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+
+text = input()
+
+unique_words = set()
+
+for i in range(n - m + 1):
+
+    word = text[i: i + m]
+
+    unique_words.add(word)
+
+print(len(unique_words))
